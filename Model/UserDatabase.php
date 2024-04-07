72c113d88815e17928972fc0d4a1f7c8b68c0dd6
JetroSulthan <jetrosulthan9626@gmail.com>
2024-04-07 21:38:52 +0700

    DB

---
+++ Model/UserDatabase.php
@@ -0,0 +1,22 @@
+<?php
+
+Class UserDatabase{ 
+
+    public $id_user;
+    public $no_telp;
+    public $owner;
+    public $status;
+
+    static function select(){
+  
+    }
+    static function insert(){
+  
+    }
+    static function update(){
+  
+    }
+    static function delete(){
+
+    }
+}
